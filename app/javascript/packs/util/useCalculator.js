24ebe60615894473c8e74c193cfa5bbc2fb01ebe
Hogan McDonald <hogan.developer@gmail.com>
2020-02-21 16:21:14 -0600

    fixes typo in useCalculator

--- app/javascript/packs/util/useCalculator.js
+++ app/javascript/packs/util/useCalculator.js
@@ -9,7 +9,7 @@ const useCalculator = () => {
     '+': 'addition',
     '-': 'subtraction',
     '*': 'multiplication',
-    '/': 'subtraction',
+    '/': 'division',
   }
 
   const validInputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
