a619f2388f146b5a5b4477c6b92316fb78e03e4d
Hogan McDonald <hogan.developer@gmail.com>
2020-02-21 16:20:25 -0600

    broadcasts to channel when equation is created

--- app/controllers/api/equations_controller.rb
+++ app/controllers/api/equations_controller.rb
@@ -1,16 +1,15 @@
 module Api
   class EquationsController < ApplicationController
     def index
-      equations = Equation.last(10).map do |equation|
-        serialize_equation(equation)
-      end
+      equations = Equation.most_recent
 
       render json: equations
     end
 
     def create
       equation = Equation.create! equation_params
-      render status: :created, json: serialize_equation(equation)
+      ActionCable.server.broadcast 'recent_equations', Equation.most_recent
+      render status: :created, json: equation
     end
 
     private
@@ -21,15 +20,6 @@ module Api
         :operand1,
         :operand2
       )
-    end
-
-    def serialize_equation(equation)
-      {
-        operand1: equation.operand1,
-        operand2: equation.operand2,
-        operator: equation.operator,
-        solution: equation.solution
-      }
     end
   end
 end
