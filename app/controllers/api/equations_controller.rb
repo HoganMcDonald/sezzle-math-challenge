ba637fcb4d5825fd244f316bf3188056a0c1d4f3
Hogan McDonald <hogan.developer@gmail.com>
2020-02-21 14:23:51 -0600

    cleans up equation apis

--- app/controllers/api/equations_controller.rb
+++ app/controllers/api/equations_controller.rb
@@ -1,6 +1,29 @@
-class EquationsController < ApplicationController
-  def index
-    equations = Equation.last(10).map do |equation|
+module Api
+  class EquationsController < ApplicationController
+    def index
+      equations = Equation.last(10).map do |equation|
+        serialize_equation(equation)
+      end
+
+      render json: equations
+    end
+
+    def create
+      equation = Equation.create! equation_params
+      render status: :created, json: serialize_equation(equation)
+    end
+
+    private
+
+    def equation_params
+      params.permit(
+        :operator,
+        :operand1,
+        :operand2
+      )
+    end
+
+    def serialize_equation(equation)
       {
         operand1: equation.operand1,
         operand2: equation.operand2,
@@ -8,23 +31,5 @@ class EquationsController < ApplicationController
         solution: equation.solution
       }
     end
-
-    render json: equations
-  end
-
-  def create
-    equation = Equation.create! equation_params
-    render status: :created, json: equation
-  end
-
-  private
-
-  def equation_params
-    params.require(:equation).permit(
-      :id,
-      :operator,
-      :operand1,
-      :operand2
-    )
   end
 end
